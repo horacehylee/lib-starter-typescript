82ee68c3525a836f8fd9b04b801efcbdc6f7bc2b
Horace Lee <horaceylee2@gmail.com>
2018-04-11 12:58:08 +0800

    migrate to jest

--- test/library.spec.ts
+++ test/library.spec.ts
@@ -1,39 +1,25 @@
-process.env.NODE_ENV = 'test';
-import 'mocha';
-import { assert, expect, should } from 'chai';
-should();
-import * as sinon from 'sinon';
-import { SinonStub } from 'sinon';
+import { Cat, Dog } from "./../src";
 
-import * as chai from 'chai';
-import * as sinonChai from 'sinon-chai';
-import * as shallowDeepEqual from 'chai-shallow-deep-equal';
-import * as chaiAsPromised from 'chai-as-promised';
-chai.use(sinonChai);
-chai.use(shallowDeepEqual);
-chai.use(chaiAsPromised);
-
-import { Cat, Dog } from './../src';
 let lib;
 
-describe('Given an instance of my Cat library', () => {
-  before(() => {
+describe("Given an instance of my Cat library", () => {
+  beforeAll(() => {
     lib = new Cat();
   });
-  describe('when I need the name', () => {
-    it('should return the name', () => {
-      expect(lib.name).to.be.equal('Cat');
+  describe("when I need the name", () => {
+    it("should return the name", () => {
+      expect(lib.name).toEqual("Cat");
     });
   });
 });
 
-describe('Given an instance of my Dog library', () => {
-  before(() => {
+describe("Given an instance of my Dog library", () => {
+  beforeAll(() => {
     lib = new Dog();
   });
-  describe('when I need the name', () => {
-    it('should return the name', () => {
-      expect(lib.name).to.be.equal('Dog');
+  describe("when I need the name", () => {
+    it("should return the name", () => {
+      expect(lib.name).toEqual("Dog");
     });
   });
 });
